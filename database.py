6c4527bd6ecfd5aa5c83d13025e401890bf7bc66
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-25 16:48:39 -0300

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
-DATABASE_URL = "postgresql://toutbox_user:14596320@localhost:5432/toutbox_db"
+DATABASE_URL = "postgresql://api2_c7jj_user:1p3QXrmGAHQFwAukULQzqK2sRrLLlsKJ@dpg-d1e4jkmmcj7s73b286l0-a/api2_c7jj"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

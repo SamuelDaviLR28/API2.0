4fc0b83c6249c97afce72bd0092ae3166d55595d
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-25 17:34:37 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,17 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
+# ⚠️ USE APENAS o valor do .env via os.getenv
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-
-DATABASE_URL = "postgresql://api2_c7jj_user:1p3QXrmGAHQFwAukULQzqK2sRrLLlsKJ@dpg-d1e4jkmmcj7s73b286l0-a/api2_c7jj"
-
+# Criação da engine do SQLAlchemy
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-
+# Dependência de sessão com o banco
 def get_db():
     db = SessionLocal()
     try:

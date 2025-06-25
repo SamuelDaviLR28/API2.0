780c1c474c29bac7f4d67359a5b434929b2cc560
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-25 16:21:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, Request, HTTPException
 from routers import dispatch, patch, rastro, cancelamento
+import os
 
 app = FastAPI(title="API Integração Transportadora - Toutbox")
 
-API_KEY = "sua-api-key-aqui"
+API_KEY = os.getenv("API_KEY")
 
 @app.middleware("http")
 async def autenticar_api_key(request: Request, call_next):
-    if request.url.path.startswith("/docs"):
+    if request.url.path.startswith(("/docs", "/openapi.json")):
         return await call_next(request)
     key = request.headers.get("x-api-key")
     if key != API_KEY:

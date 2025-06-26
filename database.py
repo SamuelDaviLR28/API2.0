5e402cd61399dd718b88a7d58d593c3691bf92fd
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-25 22:10:35 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,12 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# Função para injetar sessão do banco nas rotas
 def get_db():
     db = SessionLocal()
     try:

2e62690648740e9621ecdc3d471bfa3ff3abc095
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-25 23:02:39 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,17 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
+load_dotenv()  # Carrega as variáveis do .env
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("❌ DATABASE_URL não definida. Verifique o .env!")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Função para injetar sessão do banco nas rotas
 def get_db():
     db = SessionLocal()
     try:

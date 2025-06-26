7598fe4bfddefb1042d590a91078f2fd645c28a0
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-26 15:01:07 -0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,6 +1,9 @@
-from database import Base, engine
-from models.pedido import Pedido
+from database import engine, Base
+from models.pedido import Pedido  
 
-# Cria todas as tabelas
-Base.metadata.create_all(bind=engine)
-print("Tabelas criadas com sucesso!")
+def criar_tabelas():
+    Base.metadata.create_all(bind=engine)
+    print("Tabelas criadas com sucesso!")
+
+if __name__ == "__main__":
+    criar_tabelas()

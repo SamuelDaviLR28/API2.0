12c1c352621e98cfc28780652cc8cf8c3ac0206f
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-26 10:53:30 -0300

    Update pedido.py

--- models/pedido.py
+++ models/pedido.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, JSON
 from sqlalchemy.sql import func
-from app.database import Base
+from database import Base
+
 
 class PatchLog(Base):
     __tablename__ = "patch_logs"

a40076784cdcca057fbad59853615351b7e479b3
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-06-25 22:19:07 -0300

    Update pedido.py

--- models/pedido.py
+++ models/pedido.py
@@ -1,11 +1,11 @@
+# models/pedido.py
 from sqlalchemy import Column, Integer, String, DateTime, Text
-from sqlalchemy.sql import func
 from database import Base
 
 class Pedido(Base):
-    __tablename__ = "pedidos"
+    __tablename__ = "pedidos"  # nome da tabela no banco
 
     id = Column(Integer, primary_key=True, index=True)
-    numero_pedido = Column(String, index=True)
+    numero_pedido = Column(String(100), index=True)
     data_criacao = Column(DateTime)
     json_completo = Column(Text)

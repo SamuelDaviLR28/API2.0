ba874e7f2776f67b27e177a681c38bf5ae6a8579
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-07-01 14:51:54 -0300

    Update patch.py

--- models/patch.py
+++ models/patch.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime, JSON
 from sqlalchemy.sql import func
-from database import Base  # ou from app.database import Base, se for esse o caminho
+from database import Base  
 
 class PatchLog(Base):
     __tablename__ = "patch_logs"

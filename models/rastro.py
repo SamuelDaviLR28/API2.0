b63fc19f29a19bf77500de7921a9beebe8a808d8
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-07-07 15:05:52 -0300

    Update rastro.py

--- models/rastro.py
+++ models/rastro.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, Float
 from sqlalchemy.sql import func
-from app.database import Base
+from database import Base 
+
 
 class RastroEvent(Base):
     __tablename__ = "rastros"

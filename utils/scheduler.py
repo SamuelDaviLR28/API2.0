6871bfd87c8754bbaf0543e652fdcbc3a4b5c07b
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-07-10 16:10:56 -0300

    Update scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -5,8 +5,10 @@ from services.esl_dispatch_sender import enviar_dispatch_para_esl
 
 def start():
     scheduler = BackgroundScheduler(timezone="America/Sao_Paulo")
-    scheduler.add_job(enviar_dispatch_para_esl, 'interval', minutes=5)
-    scheduler.add_job(enviar_patches_pendentes, 'interval', minutes=5)
-    scheduler.add_job(enviar_rastros_pendentes, 'interval', minutes=5)
+    
+    scheduler.add_job(enviar_dispatch_para_esl, 'interval', minutes=5, id="dispatch_job")
+    scheduler.add_job(enviar_patches_pendentes, 'interval', minutes=5, id="patch_job")
+    scheduler.add_job(enviar_rastros_pendentes, 'interval', minutes=5, id="rastro_job")
+    
     scheduler.start()
     print("⏰ Scheduler iniciado para Dispatch, Patch e Rastro a cada 5 minutos.")

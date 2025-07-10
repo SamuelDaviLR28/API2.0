1e0b322f5db1e19e369a4ff5ba8e3c46c75686f9
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-07-10 15:54:14 -0300

    Update scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,14 +1,12 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from services.patch_sender import enviar_patches_pendentes
 from services.rastro_sender import enviar_rastros_pendentes
+from services.esl_dispatch_sender import enviar_dispatch_para_esl
 
 def start():
-    scheduler = BackgroundScheduler()
-    
-    # Envia PATCHs automaticamente a cada 5 minutos
-    scheduler.add_job(enviar_patches_pendentes, "interval", minutes=5)
-    
-    # Envia RASTROs automaticamente a cada 5 minutos
-    scheduler.add_job(enviar_rastros_pendentes, "interval", minutes=5)
-    
+    scheduler = BackgroundScheduler(timezone="America/Sao_Paulo")
+    scheduler.add_job(enviar_dispatch_para_esl, 'interval', minutes=5)
+    scheduler.add_job(enviar_patches_pendentes, 'interval', minutes=5)
+    scheduler.add_job(enviar_rastros_pendentes, 'interval', minutes=5)
     scheduler.start()
+    print("⏰ Scheduler iniciado para Dispatch, Patch e Rastro a cada 5 minutos.")

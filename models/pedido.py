79d2b3ace7e8922da2e1570369b1797fe1a045a7
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-07-22 12:12:49 -0300

    Update pedido.py

--- models/pedido.py
+++ models/pedido.py
@@ -4,10 +4,13 @@ from database import Base
 
 class Pedido(Base):
     __tablename__ = "pedidos"
-    __table_args__ = {"schema": "public"}  # usar schema public se quiser
+    __table_args__ = {"schema": "public"}
 
     id = Column(Integer, primary_key=True, index=True)
-    numero_pedido = Column(String(100), nullable=False)
+    numero_pedido = Column(String(100), nullable=False, index=True)
     cliente = Column(String(100), nullable=True)
     data_criacao = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    status = Column(String(20), default="pendente")  # pendente | enviado | erro
+    response = Column(Text, nullable=True)
     json_completo = Column(JSON, nullable=True)

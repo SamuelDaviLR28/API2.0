46b6f539586071980184de92dad3ff4f7502fcc8
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-07-22 12:19:12 -0300

    Update pedido.py

--- models/pedido.py
+++ models/pedido.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, JSON
+from sqlalchemy import Column, Integer, String, DateTime, JSON, Text
+
 from sqlalchemy.sql import func
 from database import Base
 

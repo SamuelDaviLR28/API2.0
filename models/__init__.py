3b7cd920eb370c98b4f468bba26c68770a83fc73
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-07-25 15:23:28 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,7 @@
+from .dispatch import Dispatch
+from .patch import PatchUpdate
+from .pedido import Pedido
+from .rastro import Evento
 from .sla import SLA
+from .historico_patch import HistoricoPatch
+from .historico_rastro import HistoricoRastro

dde2f1e92d18a1e135eb728e78fd553d0dd09723
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-07-25 15:23:46 -0300

    Update __init__.py

---
+++ routers/__init__.py
@@ -0,0 +1,4 @@
+from .dispatch import router as dispatch_router
+from .patch import router as patch_router
+from .rastro import router as rastro_router
+from .cancelamento import router as cancelamento_router

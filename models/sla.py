62a231e60554ae7a089fd6544d3af75e8e97fb7d
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-07-25 15:21:37 -0300

    Create sla.py

---
+++ models/sla.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class SLA(Base):
+    __tablename__ = "sla"
+    id = Column(Integer, primary_key=True, index=True)
+    uf_origem = Column(String(2), nullable=False, index=True)
+    uf_destino = Column(String(2), nullable=False, index=True)
+    cidade_destino = Column(String(100), nullable=True, index=True)
+    prazo_dias_uteis = Column(Integer, nullable=False)

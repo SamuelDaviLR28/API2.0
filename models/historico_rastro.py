c1a03b114f8b99cf1655cb703813d168df75151c
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-07-28 10:23:13 -0300

    Update historico_rastro.py

--- models/historico_rastro.py
+++ models/historico_rastro.py
@@ -1,13 +1,12 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
-from sqlalchemy.sql import func
-from database import Base
+from sqlalchemy import Column, String, Text, DateTime, func
+from app.database import Base
 
 class HistoricoRastro(Base):
     __tablename__ = "historico_rastro"
 
-    id = Column(Integer, primary_key=True, index=True)
-    nfkey = Column(String(50), nullable=False, index=True)
-    payload = Column(Text, nullable=True)
-    status = Column(String(20), nullable=True)
-    response = Column(Text, nullable=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    id = Column(String, primary_key=True, index=True)
+    nfkey = Column(String, index=True)
+    payload = Column(Text)
+    status = Column(String)
+    response = Column(Text)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())  # <- ESSA LINHA

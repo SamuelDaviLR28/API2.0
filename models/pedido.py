938a4c6a80022520cc56d44ba04a1f1864feb58b
SamuelDaviLR28 <samueldavi670@gmail.com>
2025-07-30 18:31:20 -0300

    Update pedido.py

--- models/pedido.py
+++ models/pedido.py
@@ -1,17 +1,16 @@
 from sqlalchemy import Column, Integer, String, DateTime, JSON, Text
-
 from sqlalchemy.sql import func
 from database import Base
 
 class Pedido(Base):
     __tablename__ = "pedidos"
-    __table_args__ = {"schema": "public"}
 
     id = Column(Integer, primary_key=True, index=True)
-    numero_pedido = Column(String(100), nullable=False, index=True)
-    cliente = Column(String(100), nullable=True)
+    nfkey = Column(String(44), nullable=False, unique=True, index=True)  # chave NF-e
+    uf_remetente = Column(String(2), nullable=False)
+    uf_destinatario = Column(String(2), nullable=False)
     data_criacao = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-    status = Column(String(20), default="pendente")  # pendente | enviado | erro
+    status = Column(String(20), default="pendente")
     response = Column(Text, nullable=True)
     json_completo = Column(JSON, nullable=True)
